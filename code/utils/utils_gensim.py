a1173410ccae2539fbbfa58b00992245e59b0c70
wpli <wpli@users.noreply.github.com>
2015-11-20 15:03:16 -0500

    create params

--- code/utils/utils_gensim.py
+++ code/utils/utils_gensim.py
@@ -15,13 +15,14 @@ class FolderCorpus(corpora.TextCorpus):
         self.dictionary = Dictionary()
 
         self.dictionary.add_documents(self.get_texts())
-        #self.dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=500000)
-
+        self.dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=500000)
+        self.dictionary.compactify()
 
     def get_texts(self):
         for path in self.filepaths:
             with codecs.open(path, encoding='utf8') as f:
                 raw_text = f.read()
+                raw_text = raw_text.lower()
                 for filt in self.preprocess:
                     raw_text = filt(raw_text)
                 text = list(utils.tokenize(raw_text, deacc=True, lowercase=True))
