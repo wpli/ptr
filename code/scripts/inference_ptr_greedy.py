29be3aeeadc6bf383b5ec3847c9f84c9c74aeb2e
wpli <wpli@users.noreply.github.com>
2015-11-27 20:57:55 -0500

    scripts

--- code/scripts/inference_ptr_greedy.py
+++ code/scripts/inference_ptr_greedy.py
@@ -6,13 +6,14 @@ from utils import PTRExperiment
 import logging
 import cPickle
 import collections
-        
+import copy        
 
 def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     data_directory = '../../experiments/ptr/'
 
+    # Load data
     logging.info('Loading ptr_data')
     with open(os.path.join(data_directory, 'ptr_data.p')) as f:
         ptr_data = cPickle.load(f)
@@ -23,9 +24,13 @@ def main():
 
     pfst_params = PTRExperiment.PFSTParams(ptr_data, ptr_params)
 
+    # Copy new parameters
+    logging.info('Creating new_ptr_params')
     new_ptr_params = copy.deepcopy(ptr_params)
-
     doc_ct = 0
+
+    # Inference
+    logging.info('Starting inference')
     for docid, wordids in ptr_data.docid_wordids.iteritems():
         doc_ct += 1
         if doc_ct % 1000 == 0:
@@ -37,8 +42,8 @@ def main():
             if wordids in new_ptr_params.ideas.wordids_idx_dict:
                 new_ptr_params.docid_partitions[docid][p] = new_ptr_params.ideas.wordids_idx_dict[wordids]
             else:
-                top_ideas = metrics.get_top_candidates(rand_sent, new_ptr_params, num_top_candidates=20, jaccard_cutoff=0.4)
-                assignment = metrics.get_assignment(rand_sent, top_ideas, ptr_data, new_ptr_params, pfst_params)
+                top_ideas = metrics.get_top_candidates(sent_wordids, new_ptr_params, num_top_candidates=20, jaccard_cutoff=0.5)
+                assignment = metrics.get_assignment(sent_wordids, top_ideas, ptr_data, new_ptr_params, pfst_params)
                 new_ptr_params.docid_partitions[docid][p] = assignment
 
     logging.info('Writing new_ptr_params')
